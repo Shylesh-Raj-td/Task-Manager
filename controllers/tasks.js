1a32b91ad8a9e721c330b825a1b8bc8c9220ff7d
Shylesh-Raj-td <shyleshraj2016@gmail.com>
2023-05-28 12:00:33 +0530

    Changes required

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,22 +1,52 @@
-const Task = require('../models/Task');
-const getAlltasks = (req,res) => {
-    res.send("get all items")
-}
-const createtask = async (req,res) => {
-        const task = await Task.create(req.body)
-        res.status(201).json({ task })
-}
-const gettask = (req,res) => {
-    res.json({id:req.params.id})
-}
+const Task = require('../models/Task')
+const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-error')
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({})
+  res.status(200).json({ tasks })
+})
 
-const updatetask = (req,res) => {
-    res.send("update item")
-}
-const deletetask = (req,res) => {
-    res.send("delete item")
-}
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body)
+  res.status(201).json({ task })
+})
+
+const getTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
+  const task = await Task.findOne({ _id: taskID })
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404))
+  }
+
+  res.status(200).json({ task })
+})
+const deleteTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
+  const task = await Task.findOneAndDelete({ _id: taskID })
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404))
+  }
+  res.status(200).json({ task })
+})
+const updateTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
+
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  })
+
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404))
+  }
+
+  res.status(200).json({ task })
+})
 
 module.exports = {
-    getAlltasks,createtask,gettask,updatetask,deletetask
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 }
--- routes/task.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {getAlltasks,createtask,gettask,updatetask,deletetask} = require('../controllers/tasks');
-
-router.route('/').get(getAlltasks)
-router.route('/').post(createtask)
-router.route('/:id').get(gettask).patch(updatetask).delete(deletetask)
-
-module.exports = router;
